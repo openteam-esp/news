ffb68828fb3ee4098550835770ce5a73fc81b8de
Manin Iliy <bkmzvf@gmail.com>
2011-07-11 10:32:01 +0700

    Разбил layout на auth и workspace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :get_layout
+
+  protected
+    def get_layout
+      if devise_controller?
+        'auth'
+      else
+        'workspace'
+      end
+    end
 end
