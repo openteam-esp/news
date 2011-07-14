50573134e8c5001abea64600249986e0cff6e33d
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-07-14 12:44:53 +0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Channel.destroy_all
 Channel.create! :title => "tomsk.gov.ru/news"
 Channel.create! :title => "tomsk.gov.ru/announces"
 
-Folder.create! :title => 'inbox'
-Folder.create! :title => 'published'
-Folder.create! :title => 'correcting'
-Folder.create! :title => 'draft'
-Folder.create! :title => 'trash'
+Folder.find_or_create_by :title => 'inbox'
+Folder.find_or_create_by :title => 'published'
+Folder.find_or_create_by :title => 'correcting'
+Folder.find_or_create_by :title => 'draft'
+Folder.find_or_create_by :title => 'trash'
