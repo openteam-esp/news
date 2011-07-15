2207f034be95d32b3c3aea388f3a623cbee7ec66
Manin Iliy <bkmzvf@gmail.com>
2011-07-15 16:36:41 +0700

    Не отправлять письмо если test env

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,1 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = Settings[:smtp_settings]
