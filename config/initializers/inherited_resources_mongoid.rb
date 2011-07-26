93275e3ba2578d9a153eb7922e5de4e37e2b037f
Dmitry Lihachev <lda@openteam.ru>
2011-07-26 17:22:55 +0700

    hard limit on collection size

--- config/initializers/inherited_resources_mongoid.rb
+++ config/initializers/inherited_resources_mongoid.rb
@@ -1,6 +1,6 @@
 module MongoidActions
   def collection
-    get_collection_ivar || set_collection_ivar(end_of_association_chain.all)
+    get_collection_ivar || set_collection_ivar(end_of_association_chain.limit(20))
   end
 end
 
