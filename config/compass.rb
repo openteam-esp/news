3b20272a1611624e7c844b2347eb127d43b49dbe
Evgeny Lapin <lev@openteam.ru>
2011-08-01 16:14:27 +0700

    +fancy-buttons

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,7 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
+require 'fancy-buttons'
+
 project_type = :rails
 
 # Set this to the root of your project when deployed:
@@ -14,3 +16,4 @@ output_style = :compressed
 line_comments = false
 
 preferred_syntax = :sass
+
