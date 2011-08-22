e28cd1c5fb597ff625fa199f987c4418c908ffa9
Evgeny Lapin <lev@openteam.ru>
2011-08-22 12:57:00 +0700

    обвязка для аплоадера со стороны контроллера

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -18,4 +18,18 @@ class EntriesController < AuthorizedApplicationController
     create! { edit_folder_entry_path(@entry.folder, @entry) }
   end
 
+  def update
+    update! do |success, failure|
+      success.html {
+        if request.xhr?
+          @entry.reload
+          @entry.assets.build
+          render :edit, :layout => false and return
+        end
+        redirect_to smart_resource_url
+      }
+    end
+  end
+
 end
+
