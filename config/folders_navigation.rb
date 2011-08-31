9aad2a1d65d9e87315e025cc0d9eeb8104a09a5a
Evgeny Lapin <lev@openteam.ru>
2011-08-31 10:57:07 +0700

    исправлен js-механизм отображения истории изменений

--- config/folders_navigation.rb
+++ config/folders_navigation.rb
@@ -2,9 +2,6 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :inbox, "#{t('inbox')}", messages_path, :counter => @current_user.messages.count
 
-    primary.item :awaiting_correction, t('awaiting_correction'), entries_path(:state => 'awaiting_correction'),
-                 :highlights_on => lambda { params[:state] == :awaiting_correction } if @current_user.corrector?
-
     %w[draft awaiting_correction correcting awaiting_publication published trash].each do | state |
       primary.item state, t(state), "/#{state}#{entries_path}",
                    :highlights_on => lambda { params[:state] == state || (params[:controller] == 'entries' && @entry.try(:state) == state) } #  if @current_user.corrector?
