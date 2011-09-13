788949b0552e66c883dbba9b4f698fb6cd81dc75
Dmitry Lihachev <lda@openteam.ru>
2011-09-13 10:05:02 +0700

    возобновить работу над задачей

--- app/models/issues/prepare.rb
+++ app/models/issues/prepare.rb
@@ -2,18 +2,13 @@ class Prepare < Issue
   state_machine :initial => :processing do
     state :processing
     state :completed
-    state :pending
 
     event :complete do
       transition :processing => :completed
     end
 
-    event :discard do
-      transition :processing => :pending
-    end
-
     event :restore do
-      transition :pending => :processing
+      transition :completed => :processing
     end
   end
 end
