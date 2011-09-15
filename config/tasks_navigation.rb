ed008788715da721472c3bd40bee458f512b4648
Dmitry Lihachev <lda@openteam.ru>
2011-09-15 12:29:09 +0700

    не выводить counter в сайдбаре на созданных мною задачах

--- config/tasks_navigation.rb
+++ config/tasks_navigation.rb
@@ -2,7 +2,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
 
     %w[fresh processed_by_me initiated_by_me].each do | scope |
-      primary.item "sidebar_tasks_#{scope}", t("sidebar.tasks.#{scope}"), tasks_path(scope), :counter => Task.send(:kind, scope).count,
+      primary.item "sidebar_tasks_#{scope}", t("sidebar.tasks.#{scope}"),
+                   tasks_path(scope), :counter => scope == 'initiated_by_me' ? 0 : Task.send(:kind, scope).count,
                    :highlights_on => lambda { params[:kind] == scope && params[:controller] == 'tasks' }
     end
 
