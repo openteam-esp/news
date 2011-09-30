c66f912181b07a3deb935e4b711bdf6f6c11aa73
Dmitry Lihachev <lda@openteam.ru>
2011-09-30 12:58:56 +0700

    provide bucket_name throught environment variable

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -3,6 +3,7 @@ Settings.read Rails.root.join 'config', 'settings.yml'
 
 Settings.define 's3.access_key_id',           :env_var => 'S3_ACCESS_KEY_ID'
 Settings.define 's3.secret_access_key',       :env_var => 'S3_SECRET_ACCESS_KEY'
+Settings.define 's3.bucket_name',             :env_var => 'S3_BUCKET_NAME'
 
 Settings.define 'hoptoad.api_key',            :env_var => 'HOPTOAD_API_KEY'
 Settings.define 'hoptoad.host',               :env_var => 'HOPTOAD_HOST'
