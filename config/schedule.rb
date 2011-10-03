50e0d9e15394604360b572adb96b5241df322684
Dmitry Lihachev <lda@openteam.ru>
2011-10-03 14:52:29 +0700

    remove stale entries with clockwork

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 require File.expand_path('../boot', __FILE__)
 require File.expand_path('../environment', __FILE__)
 
-every(1.days, 'trash.clean', :at => "00:00")  { Entry.trash.where("created_at < ?", Time.now - 30.days).destroy_all }
+every(1.minutes, 'trash.clean')  { Entry.stale.delete_all }
