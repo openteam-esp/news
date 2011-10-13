929298dfa6c896c94b8ed4f7ac8ccd90bce8aab6
Evgeny Lapin <lev@openteam.ru>
2011-10-13 16:09:55 +0700

    unscape file_name in middleware

--- vendor/plugins/authorize_assets_middleware/lib/authorize_assets_middleware.rb
+++ vendor/plugins/authorize_assets_middleware/lib/authorize_assets_middleware.rb
@@ -5,7 +5,7 @@ class AuthorizeAssetsMiddleware
 
   def call(env)
     if env['PATH_INFO'] =~ %r{^/assets/(\d+)/(?:(?:\d+-\d+|cropped)/)?(.*)}
-      throw(:warden) unless Ability.new.can? :read, Asset.where(:file_name => $2).find($1)
+      throw(:warden) unless Ability.new.can? :read, Asset.where(:file_name => Rack::Utils.unscape($2)).find($1)
     end
     @app.call(env)
   end
