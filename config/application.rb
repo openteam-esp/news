30611eb526dcbb5ba97314e859c2a0fe073543de
Dmitry Lihachev <lda@openteam.ru>
2012-02-08 11:16:45 +0700

    rails 3.2 configs

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module News
                                 #{config.root}/lib
                                )
 
-    #config.autoload_paths += %W[#{config.root}/lib/legacy_models] unless Rails.env.production?
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -45,20 +43,26 @@ module News
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    # config.active_record.whitelist_attributes = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.generators do | generators |
-      generators.test_framework       :rspec, :fixture => true
-      generators.fixture_replacement  :fabrication
-    end
-
+    # Load Task to resolve Rake's Task conflict
     config.to_prepare do
-      require_dependency Rails.root.join("app/models/tasks/task").to_s
+      require_dependency Rails.root.join("app/models/tasks/task.rb").to_s
     end
   end
 end
-
--- config/initializers/setup_mail.rb
+++
@@ -1,2 +0,0 @@
-ActionMailer::Base.smtp_settings = Settings[:smtp_settings]
-ActionMailer::Base.default_url_options[:host] = Settings[:host]
