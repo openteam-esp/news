132d408fb6880c19c3418e16668e60255fda5fff
Dmitry Lihachev <lda@openteam.ru>
2012-02-17 09:02:02 +0700

    #9290. текстовая версия интервала мероприятия

--- app/models/entries/event_entry_property.rb
+++ app/models/entries/event_entry_property.rb
@@ -1,4 +1,40 @@
+#encoding: utf-8
+
 class EventEntryProperty < ActiveRecord::Base
+  validates_presence_of :since, :until
+
+  def interval
+    if since_date == until_date
+      if since_time == until_time
+        if since_time == "00:00"
+          since_date
+        else
+          "#{since_date} в #{since_time}"
+        end
+      else
+        "#{since_date} с #{since_time} по #{until_time}"
+      end
+    else
+      "с #{since_date} по #{until_date}"
+    end
+  end
+
+  def as_json(options={})
+    super(options.merge(:methods => :interval))
+  end
+
+  private
+    %w[since until].each do |field|
+      define_method "localized_#{field}" do
+        I18n.l(self.send(field))
+      end
+      define_method "#{field}_date" do
+        self.send("localized_#{field}").split(' ').first
+      end
+      define_method "#{field}_time" do
+        self.send("localized_#{field}").split(' ').second
+      end
+    end
 end
 # == Schema Information
 #
