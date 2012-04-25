54f5243a47557a8da1c1890d4a831477115e1361
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-04-25 11:28:00 +0700

    typo fix

--- app/models/entries/news_entry.rb
+++ app/models/entries/news_entry.rb
@@ -1,4 +1,8 @@
 class NewsEntry < Entry
+  # NOTE: используется для методов min_since_event_datetime и max_until_event_datetime в #EntrySearch
+  def event_entry_properties
+    []
+  end
 end
 
 # == Schema Information
