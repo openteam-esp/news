4d728679e685115324f85b1ec8a3404784d08624
Dmitry Lihachev <lda@openteam.ru>
2012-12-28 08:59:06 +0700

    count only uniq news/tasks in folder placeholders

--- config/entries_navigation.rb
+++ config/entries_navigation.rb
@@ -2,7 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
 
     %w[draft processing deleted].each do | folder |
-      primary.item "sidebar_entry_#{folder}", t("sidebar.entries.#{folder}"), manage_news_scoped_entries_path(folder), :counter => Entry.folder(folder, current_user).count,
+      primary.item "sidebar_entry_#{folder}", t("sidebar.entries.#{folder}"), manage_news_scoped_entries_path(folder), :counter => Entry.folder(folder, current_user).count('DISTINCT entries.id'),
                    :highlights_on => lambda { params[:folder] == folder && params[:controller] == 'manage/news/entries' }
     end
 
--- config/tasks_navigation.rb
+++ config/tasks_navigation.rb
@@ -3,7 +3,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     %w[fresh processed_by_me initiated_by_me].each do | folder |
       primary.item "sidebar_tasks_#{folder}", t("sidebar.tasks.#{folder}"),
-                   manage_news_tasks_path(folder), :counter => folder == 'initiated_by_me' ? 0 : Task.folder(folder, current_user).count,
+                   manage_news_tasks_path(folder), :counter => folder == 'initiated_by_me' ? 0 : Task.folder(folder, current_user).count('DISTINCT tasks.id'),
                    :highlights_on => lambda { params[:folder] == folder && params[:controller] == 'manage/news/tasks' }
     end
 
