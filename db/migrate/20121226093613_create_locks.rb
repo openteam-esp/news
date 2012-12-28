f625802ef5861727a6287bdbfc71c02feaa4eba0
Dmitry Lihachev <lda@openteam.ru>
2012-12-28 12:22:33 +0700

    remove unused lock fields form entries

--- db/migrate/20121226093613_create_locks.rb
+++ db/migrate/20121226093613_create_locks.rb
@@ -1,5 +1,5 @@
 class CreateLocks < ActiveRecord::Migration
-  def change
+  def up
     create_table :locks do |t|
       t.references :entry
       t.references :user
@@ -8,5 +8,14 @@ class CreateLocks < ActiveRecord::Migration
     end
     add_index :locks, :entry_id, :uniq => true
     add_index :locks, :user_id
+    require 'timecop'
+    Entry.where('locked_by_id IS NOT NULL').each do |entry|
+      Timecop.freeze(entry.attributes['locked_at']) do
+        entry.current_user = User.find(entry.attributes['locked_by_id'])
+        entry.lock
+      end
+    end
+    remove_column :entries, :locked_by_id
+    remove_column :entries, :locked_at
   end
 end
