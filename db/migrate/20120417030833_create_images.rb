6c20b337b64db9a39ae72e563ee711fbfd117d98
Dmitry Lihachev <lda@openteam.ru>
2013-03-12 11:38:34 +0700

    create_images must be reversable

--- db/migrate/20120417030833_create_images.rb
+++ db/migrate/20120417030833_create_images.rb
@@ -21,15 +21,6 @@ class CreateImages < ActiveRecord::Migration
   def down
     add_column :entries, :image_url, :string
     add_column :entries, :image_description, :string
-
-    Entry.all.each do |entry|
-      image = entry.images.first
-
-      entry.update_attributes! :image_url => image.url, :image_description => image.description if image
-    end
-
-    remove_index :images, :entry_id
-
     drop_table :images
   end
 end
