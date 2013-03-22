7de235fed8467b030841fae52425386a9abaa074
Dmitriy Baraksanov <bdn@openteam.ru>
2013-03-22 15:34:29 +0700

    update rails & brakeman

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-News::Application.config.secret_token = '596c2eb3a5c25b8e9c0ca4b857b22eb939ee5e8b915fa01cee9b6014b646a83782c292266de38785b3ab41a16df5987b1e50f1b0b86b924e4588c1a49b2932cd'
+#News::Application.config.secret_token = '596c2eb3a5c25b8e9c0ca4b857b22eb939ee5e8b915fa01cee9b6014b646a83782c292266de38785b3ab41a16df5987b1e50f1b0b86b924e4588c1a49b2932cd'
