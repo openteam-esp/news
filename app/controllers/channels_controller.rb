9e627a1115a43a97784c18dca729e49410e91db9
Evgeny Lapin <lev@openteam.ru>
2013-11-29 12:12:03 +0700

    support cap task sunspot:pull, channel#show permanently redirect to root if request html format

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,4 +2,10 @@ class ChannelsController < ApplicationController
   inherit_resources
   actions :index, :show
   respond_to :json
+
+  def show
+    show! do |format|
+      format.html { redirect_to root_url, :status => 301 }
+    end
+  end
 end
