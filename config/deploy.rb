01339fe4870cb630a9dff02a9d59d1311ad2972c
Evgeny Lapin <lev@openteam.ru>
2015-12-10 10:52:46 +0600

    updated sunspot_rails; added to shared sunspot.yml in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,5 @@ require 'whenever/capistrano'
 require 'sidekiq/capistrano'
 
 set :default_stage, 'ato'
+
+set :shared_children, fetch(:shared_children) + %w[config/sunspot.yml]
--- config/initializers/yt_config.rb
+++ config/initializers/yt_config.rb
@@ -1,1 +1,4 @@
-Yt.configuration.api_key = Settings['youtube.api_key']
+Yt.configure do |config|
+  config.log_level = :debug if Rails.env.development?
+  config.api_key = Settings['youtube.api_key']
+end
